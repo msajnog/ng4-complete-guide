02a49c0040d5961362c9ca4aaff46ff30e510d6f
Mateusz Sajnóg <mateusz.sajnog@britenet.com.pl>
2017-11-21 12:00:50 +0100

    add angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { CoreModule } from './core/core.module';
     ShoppingListModule,
     SharedModule,
     AuthModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   bootstrap: [AppComponent],
 })
