efb36ec93f9c1024c61f4143f338cef89db93575
Mateusz Sajnóg <mateusz.sajnog@britenet.com.pl>
2017-11-21 11:54:28 +0100

    universal final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     AppComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'my-universal-app'}),
     HttpClientModule,
     AppRoutingModule,
     ShoppingListModule,
